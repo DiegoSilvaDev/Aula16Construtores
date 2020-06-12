fdf34f187e564240f93485f1580f412db776f312
DiegoSilvaDev <Diego2102.ds@gmail.com>
2020-06-12 14:32:31 -0300

    Ok

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MetodoConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instância sem argumentos
+            Produto sapato = new Produto();
+            sapato.Nome  = "Vizzano";
+        
+            // Instância com 1 argumento
+            Produto chinelo = new Produto(45);
+            Console.WriteLine(chinelo.Codigo);
+        
+            // Instância com todos os atributos como argumentos 
+            Produto camisa = new Produto(34, "Supreme","Azul", 234);
+            System.Console.WriteLine(camisa.Codigo);
+            System.Console.WriteLine(camisa.Nome);
+            System.Console.WriteLine(camisa.Descricao);
+            System.Console.WriteLine(camisa.Estoque);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
